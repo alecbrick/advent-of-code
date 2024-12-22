7956a3eb5fc4ffd089161139bf57c371c586ca84
Alec Brickner <alec@primer.ai>
2024-12-22 01:04:08 -0800

    Day 12 and 22

--- 2024/src/lib.rs
+++ 2024/src/lib.rs
@@ -6,3 +6,23 @@ pub mod utils;
 pub fn pos_out_of_bounds(pos: (i32, i32), height: i32, width: i32) -> bool {
     pos.0 < 0 || pos.1 < 0 || pos.0 >= height || pos.1 >= width
 }
+
+pub fn turn_left(dir: (i32, i32)) -> (i32, i32) {
+    match dir {
+        (1, 0) => (0, 1),
+        (0, 1) => (-1, 0),
+        (-1, 0) => (0, -1),
+        (0, -1) => (1, 0),
+        _ => panic!("bad dir")
+    }
+}
+
+pub fn turn_right(dir: (i32, i32)) -> (i32, i32) {
+    match dir {
+        (1, 0) => (0, -1),
+        (0, 1) => (1, 0),
+        (-1, 0) => (0, 1),
+        (0, -1) => (-1, 0),
+        _ => panic!("bad dir")
+    }
+}
